16f831c2ec62460a7d828dd6815e69263e7138e0
Al3xis <alexxxfernandez50@gmail.com>
2024-10-31 00:48:44 -0300

    creating_folders

---
+++ main.py
@@ -0,0 +1,16 @@
+import sys
+from PyQt5.QtWidgets import QApplication, QMainWindow
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle("Point of Sale")
+        self.setGeometry(100, 100, 800, 600)  # Set window position and size
+
+        # Add any additional setup code here
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    main_window = MainWindow()
+    main_window.show()
+    sys.exit(app.exec_())
